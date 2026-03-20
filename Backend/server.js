d7986cbabbac6acf5dd8deeba6e72f9ffd22241d
Agrajsingh <143416360+Agrajsingh@users.noreply.github.com>
2026-03-20 21:10:10 +0530

    Debug: Add request logging to backend

--- Backend/server.js
+++ Backend/server.js
@@ -9,6 +9,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// Logging middleware
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
 // Connect to MongoDB
 mongoose
   .connect(
